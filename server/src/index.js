6489951f07420a3dc8e7009ac73af45ce829d0e8
Phytertek <ryan.phytertek@gmail.com>
2017-06-04 00:01:35 -0400

    added logger to index.js, replaced console.log statements with logger

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,13 @@
-// import app
+// our packages
 import app from './app'
+import logger from './util/logger'
 
 // start server
 app.listen(8080, function() {
   const host = this.address().address
   const port = this.address().port
-  console.log(`Shard listening on http://${host}:${port}`)
+  logger.log(`Shard listening on http://${host}:${port}`)
 })
 
-process.on('uncaughtException', err => console.error('Uncaught exception:', err))
-process.on('unhnadledRejection', error => console.error('Unhandled rejection:', error))
+process.on('uncaughtException', err => logger.error('Uncaught exception:', err))
+process.on('unhnadledRejection', error => logger.error('Unhandled rejection:', error))
